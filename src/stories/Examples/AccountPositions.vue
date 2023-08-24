6d0de56382acb11ac4c9bb5fbd9645e894ec595b
eseyfried <eric.seyfried@gmail.com>
2023-08-24 06:52:18 -0400

    Added source code component

--- src/stories/Examples/AccountPositions.vue
+++ src/stories/Examples/AccountPositions.vue
@@ -24,46 +24,80 @@
             <Column header="Actions" field="action" type="actions" :contextMenuOptions="contextMenuOptions" />
         </PositionsGrid>
     </div>
+    <SFCSource filename="AccountPositions.vue" tag="all" />
 </template>
 <script setup>
-import { ref } from "vue";
+/**
+ * Use a reactive data store to manage your application data.
+ * You can centralize your api calls within the data layer.
+ */
 import { storeToRefs } from "pinia";
 import { useDemoStore } from "@/stories/stores/store";
+import SFCSource from '@/stories/Examples/SFCSource.vue';
+/**
+ * Import your components
+ */
 import Header from '@/stories/Examples/Header.vue';
 import AccountCarousel from '@/components/Core/Navigation/AccountCarousel.vue';
 import PositionsGrid from '@/components/Core/PositionsGrid/PositionsGrid.vue';
 import Column from '@/components/DataTable/Column.vue';
 import ColumnFilter from '@/components/DataTable/ColumnFilter/ColumnFilter.vue';
-const { positions, accounts } = storeToRefs(useDemoStore());
+/**
+ * Import modules, libraries, etc.
+ */
+// Here we use faker-js to generate some fake data for demonstration purposes
 import { faker } from "@faker-js/faker";
+/**
+ * Let's grab some data from our data store to pass into our components
+ */
+const { positions, accounts } = storeToRefs(useDemoStore());
 
+/**
+ * The PositionsGrid component offers a QuoteHover option.
+ * To use that option we must supply a callback that provides a
+ * quote data object given a symbol. Here is a mock callback that
+ * simulates making an async call to an api and returns a quote object.
+ */
 const fetchQuote = async (symbol) => {
-            console.log("fetching quote for %s", symbol)
-            return new Promise(resolve => {
-                const quote = {}
-                setTimeout(function() { 
-                    quote.showDetails = true;
-                    quote.symbol = symbol;
-                    quote.security_description = faker.company.name();
-                    quote.current_price = 191.94 * Math.random();
-                    quote.price_change_amt = 0.81;
-                    quote.price_change_pct = 0.42;
-                    quote.previous_close_price = 191.94;
-                    quote.day_low_price =  192.25;
-                    quote.day_high_price = 194.91;
-                    quote.exchange = "NASDAQ";
-                    quote.volume = "5.38M";
-                    quote.bid = 192.75;
-                    quote.ask = 191.75;
-                    quote.pe_ratio = 32.75;
-                    quote.dividend_yield = 0.50;
-                    resolve(quote)
-                }, 2000);
-            });
-        }
+    console.log("fetching quote for %s", symbol)
+    return new Promise(resolve => {
+        const quote = {}
+        // simulate making an async call
+        setTimeout(function() { 
+            // construct quote object with sample data
+            quote.showDetails = true;
+            quote.symbol = symbol;
+            quote.security_description = faker.company.name();
+            quote.current_price = 191.94 * Math.random();
+            quote.price_change_amt = 0.81;
+            quote.price_change_pct = 0.42;
+            quote.previous_close_price = 191.94;
+            quote.day_low_price =  192.25;
+            quote.day_high_price = 194.91;
+            quote.exchange = "NASDAQ";
+            quote.volume = "5.38M";
+            quote.bid = 192.75;
+            quote.ask = 191.75;
+            quote.pe_ratio = 32.75;
+            quote.dividend_yield = 0.50;
+            // return quote object
+            resolve(quote);
+        }, 2000);
+    });
+}
+/**
+ * The PositionsGrid accepts a quoteDetailsOptions property with
+ * configuration options for the QuoteHover component. Here we
+ * configure it with out callback.
+ */
 const quoteDetailOptions = {
     callback: fetchQuote,
 }
+/**
+ * The PositionsGrid component offers an option to use a Column component
+ * configured as an action menu column. This requires passing a contextMenuOptions
+ * object. Here we configure the menu items for the action menu.
+ */
 const contextMenuOptions = {
     menuItems: [
             { url: null, label: "Market News" },
@@ -71,6 +105,11 @@ const contextMenuOptions = {
             { url: null, label: "Trade", callback: (item, rowData) => { console.log(item), console.log(rowData) } },
         ]
 }
+/**
+ * The AccountCarousel component fires an event when an account is clicked.
+ * Here we are binding an event handler to that event where we can decide what
+ * action to take. In this trivial example, we simply alert the account number.
+ */
 const handleAccountClick = (event) => {
     const { account } = event;
     window.alert(`Account: ${account.number} was selected`)
---
+++ src/stories/Examples/SFCSource.vue
@@ -0,0 +1,105 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <button @click="showSource = !showSource">Show Source Code</button>
+        <prism v-if="sourceCode && showSource" language="javascript">{{ sourceCode }}</prism>
+    </div>
+</template>
+<script setup>
+import { ref, computed } from "vue";
+
+import "prismjs";
+import "prismjs/themes/prism-funky.css";
+import Prism from 'vue-prism-component';
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+    filename: {
+        type: String,
+        default: null,
+        required: true
+    },
+    tag: {
+        type: String,
+        default: null,
+        validator: (value) => ["template", "script", "style"].includes(value)
+    },
+    
+});
+const rawSource = ref(null);
+const showSource = ref(false);
+
+const importedSource = () => import(/* @vite-ignore */ `../Examples/${props.filename}?raw`);
+importedSource().then((resolve) => {
+        rawSource.value = resolve.default;
+})
+const sourceCode = computed(() => {
+    if(rawSource.value) {
+        switch(props.tag) {
+            default:
+                return stripSourceComponent(rawSource.value);
+            case "template":
+                return getTemplate(rawSource.value);
+            case "script":
+                return getScript(rawSource.value);
+            case "style":
+                return getStyle(rawSource.value);
+        }
+    }
+    return ""
+});
+
+const getTemplate = (code) => {
+    const tagStart = code.indexOf("<template>");
+    const tagEnd = code.indexOf("</template>") + "</template>".length;
+    const sourceCode = code.substring(tagStart, tagEnd);
+    return stripSourceComponent(sourceCode);
+}
+const getScript = (code) => {
+    const tagStart = code.indexOf("<script");
+    const tagEnd = code.indexOf("/script>") + "</sctipt>".length;
+    const sourceCode = code.substring(tagStart, tagEnd);
+    return stripSourceComponent(sourceCode);
+}
+const getStyle = (code) => {
+    const tagStart = code.indexOf("<style");
+    const tagEnd = code.indexOf("/style>") + "</style>".length;
+    const sourceCode = code.substring(tagStart, tagEnd);
+    return stripSourceComponent(sourceCode);
+}
+const stripSourceComponent = (code) => {
+    const regex = /[ \t]*?<SFCSource(.*)\/>\n?/g
+    return code.replace(regex,"");
+}
+</script>
+<style lang="scss" scoped>
+pre[class*="language-"] {
+    background: #000000;
+}
+button {
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    background: transparent;
+    border: none;
+    border-radius: 4px;
+    color: inherit;
+    cursor: pointer;
+    display: -webkit-inline-box;
+    display: -webkit-inline-flex;
+    display: -ms-inline-flexbox;
+    display: inline-flex;
+    font-size: 13px;
+    font-weight: bold;
+    height: 28px;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    margin-top: 6px;
+    margin-left: 1rem;
+    padding: 1rem 1rem;
+    background-color: #029CFD;
+    color: #E5F4FF;
+}
+</style>

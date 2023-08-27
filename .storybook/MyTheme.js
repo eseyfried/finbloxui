2a3011567a1dce800abe56dbcc659a6fc06d2ee2
eseyfried <eric.seyfried@gmail.com>
2023-08-27 08:36:14 -0400

    added finbloxui logo

--- .storybook/MyTheme.js
+++ .storybook/MyTheme.js
@@ -1,9 +1,10 @@
 import { create } from '@storybook/theming/create';
-
+import logo from '../public/assets/logo-400.png';
 export default create({
   base: 'light',
-  brandTitle: 'My custom Storybook',
-  brandUrl: 'https://example.com',
-  brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandTitle: 'FinBlox-UI',
+  brandUrl: 'https://finbloxui.com',
+  brandImage: logo,
+//   brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
   brandTarget: '_self',
 });

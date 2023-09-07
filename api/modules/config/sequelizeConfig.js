c3f04389fec47232a9ef33be061c639182df76f2
eseyfried <eric.seyfried@gmail.com>
2023-09-07 12:27:24 -0400

    added env file creation

--- api/modules/config/sequelizeConfig.js
+++ api/modules/config/sequelizeConfig.js
@@ -1,14 +1,21 @@
 
-// const { config } = require(__dirname + '/config.js');
-// const path = require('path');
+const { config } = require(__dirname + '/config.js');
+const path = require('path');
 
 module.exports = {
+    // "development": {
+    //     "username": "finbloxui",
+    //     "password": "pass22pass",
+    //     "database": "finbloxui",
+    //     "host": "finbloxui-db",
+    //     "dialect": "mysql",
+    // },
     "development": {
-        "username": "finbloxui",
-        "password": "pass22pass",
-        "database": "finbloxui",
-        "host": "finbloxui-db",
-        "dialect": "mysql",
+        "username": config.db_user,
+        "password": config.db_pwd,
+        "database": config.db_name,
+        "host": config.db_host,
+        "dialect": config.db_dialect
     },
     // "test": {
     //     "username": config.db_user,

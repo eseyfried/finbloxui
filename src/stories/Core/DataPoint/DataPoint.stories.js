aa14a074e346b5ad3a6c80e807afe9be06427d0f
eseyfried <eric.seyfried@gmail.com>
2024-01-25 07:02:38 -0500

    updated DataPoint story using simplified approach

--- src/stories/Core/DataPoint/DataPoint.stories.js
+++ src/stories/Core/DataPoint/DataPoint.stories.js
@@ -1,7 +1,49 @@
-import { DataPointRoot } from '@/components/Core/DataPoint/';
+import {
+    DataPointRoot,
+    DataPointLabel,
+    DataPointValue,
+    DataPointTrend,
+    DataPointAction
+} from '@/components/Core/DataPoint/';
 import { action } from "@storybook/addon-actions";
-import { useStoryTemplate } from "@/stories/modules/useStoryTemplate";
+import { defaultValue, generateArgs } from "@/stories/modules/useStoryHelper";
 
+const getArgs = generateArgs({
+    as: defaultValue(DataPointRoot, 'as'),
+    dataPoint: {
+        label: "YTD AUM",
+        value: "12345678.90",
+        format: "currency",
+        trend: {
+            format: "currency",
+            value: 14526.34,
+            direction: "up"
+        }
+    },
+    actionLabel: defaultValue(DataPointRoot, 'actionLabel'),
+    showAction: defaultValue(DataPointRoot, 'showAction'),
+})
+
+const components = { 
+    DataPointRoot,
+    DataPointLabel,
+    DataPointValue,
+    DataPointTrend,
+    DataPointAction
+ }
+
+const template = `
+    <DataPointRoot
+        :dataPoint="args.dataPoint"
+        :actionLabel="args.actionLabel"
+        :showAction="args.showAction"
+        :as="args.as"
+        @fb-data-point-action-link:click="handleActionClick"
+    />`
+
+const methods = {
+    handleActionClick: (datapoint) => { alert(`Action link was clicked with data ${JSON.stringify(datapoint)}`); action('@fb-data-point-action-link:click')(datapoint) }
+}
 
 /**
  * ## Overview
@@ -12,24 +54,11 @@ import { useStoryTemplate } from "@/stories/modules/useStoryTemplate";
  * > As a user, I want to see important summary statistics so that I can get a quick sense of how my portfolio or account is doing.
  */
 
-const main =  {
+export default  {
     title: 'Library/Core/DataPoint',
-    components: { DataPointRoot },
+    components,
     tags: ['autodocs'],
-    args: {
-        dataPoint: {
-            label: "YTD AUM",
-            value: "12345678.90",
-            format: "currency",
-            trend: {
-                format: "currency",
-                value: 14526.34,
-                direction: "up"
-            }
-        },
-        actionLabel: "show more",
-        showAction: true,
-    },
+    args: getArgs(),
     argTypes: {
         dataPoint: {
             type: { name: "Object", required: true },
@@ -45,7 +74,7 @@ const main =  {
             description: "Show action link.",
             table: {
                 type: { summary: "Boolean" },
-                defaultValue: { summary: "true" },
+                defaultValue: { summary: defaultValue(DataPointRoot, 'showAction') },
                 category: 'Props',
             },
             options: [true, false],
@@ -55,11 +84,9 @@ const main =  {
             description: "Text label for action link",
             table: {
                 type: { summary: "String" },
-                defaultValue: { summary: "Show More" },
+                defaultValue: { summary: defaultValue(DataPointRoot, 'actionLabel') },
                 category: 'Props',
             },
-            type: { name: "string" },
-            defaultValue: "Show More",
             control: { type: 'text' },
         },
         "fb-data-point-action-link:click": {
@@ -92,65 +119,96 @@ const main =  {
             table: {
                 category: 'CSS Classes',
             },
-        }
+        },
+        DataPointLabel: {
+            table: {
+                type: { summary: "Sub-Component" },
+                defaultValue: { summary: '<DataPointLabel />' },
+                category: 'Related Components',
+            },
+        },
+        DataPointValue: {
+            table: {
+                type: { summary: "Sub-Component" },
+                defaultValue: { summary: '<DataPointValue />' },
+                category: 'Related Components',
+            },
+        },
+        DataPointAction: {
+            table: {
+                type: { summary: "Sub-Component" },
+                defaultValue: { summary: '<DataPointAction />' },
+                category: 'Related Components',
+            },
+        },
+        DataPointTrend: {
+            table: {
+                type: { summary: "Sub-Component" },
+                defaultValue: { summary: '<DataPointTrend />' },
+                category: 'Related Components',
+            },
+        },
     }
 };
 
-export default main;
-
-const template = `<DataPointRoot v-bind="args" @fb-data-point-action-link:click="handleActionClick" />`
-
-const Template = (config) => {
-    return {
-        render: (args) => {
-            return {
-                components: { DataPointRoot },
-                setup() {
-                    return { args };
-                },
-                template: config?.template || useStoryTemplate(template, config?.props),
-                methods: {
-                    handleActionClick: (datapoint) => { alert(`Action link was clicked with data ${JSON.stringify(datapoint)}`); action('@fb-data-point-action-link:click')(datapoint) }
-                }
-            }
-        },
-        parameters: {
-            docs: {
-                description: {
-                    story: config?.story,
-                },
-                source: {
-                    code: config?.template || config?.source || useStoryTemplate(template, config?.props)
-                }
-            }
-        }
-    };
-}
 
 
-export const DefaultClientCard = {
-    ...Template({
-        story: "The default `DataPointRoot`component renders a data point's label, value, trend and action link.",
-        props: { ...main.args }
-    })
-}
+const render = (args) => ({
+    components,
+    setup: () => { return { args } },
+    template,
+    methods
+})
+
+
+export const BasicUse = render
 
 export const WithCustomActionLabel = {
-    ...Template({
-        props: { ...main.args, actionLabel: "show more details" }
-    }),
+    render,
     args: {
-        ...main.args,
         actionLabel: "show more details"
-    },
+    }
 }
 
 export const WithActionOff = {
-    ...Template({
-        props: { ...main.args, showAction: false }
-    }),
+    render,
     args: {
-        ...main.args,
         showAction: false
-    },
+    }
 }
+
+/**
+ * 
+ * You can create a custom layout using sub-components and wrapping them with your own HTML tags and css.
+ */
+export const CustomLayoutUsingSubComponents = (args) => ({
+    components,
+    setup: () => { return { args } },
+    methods,
+    template: `
+    <div class="custom-layout">
+    <DataPointRoot :dataPoint="args.dataPoint" class="w-1/2">
+        <div class="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
+            <div class="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-green-600 to-green-400 text-white shadow-green-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-6 h-6 text-white">
+                    <path d="M12 7.5a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"></path>
+                    <path fill-rule="evenodd" d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 14.625v-9.75zM8.25 9.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM18.75 9a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V9.75a.75.75 0 00-.75-.75h-.008zM4.5 9.75A.75.75 0 015.25 9h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H5.25a.75.75 0 01-.75-.75V9.75z" clip-rule="evenodd"></path>
+                    <path d="M2.25 18a.75.75 0 000 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 00-.75-.75H2.25z"></path>
+                </svg>
+            </div>
+            <div class="p-4 text-right">
+                <DataPointLabel as="p" class="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600" />
+                <DataPointValue class="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900" />
+            </div>
+            <div class="border-t border-blue-gray-50 p-4">
+                <p class="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
+                    <strong class="text-green-500">
+                        <DataPointTrend />
+                    </strong>&nbsp;since yesterday
+                </p>
+            </div>
+        </div>
+    </DataPointRoot>
+    </div>
+    `
+})
